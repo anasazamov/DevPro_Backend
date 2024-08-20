d01a060d28fb491939d1e00fffd91e910144a20d
anasazamov <anasazamov55@gmail.com>
2024-08-20 18:59:37 +0500

    parsing website

--- parsing_website.py
+++ parsing_website.py
@@ -1,51 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
-# Saytdan mahsulotlarni olish
-def get_products_from_olcha():
-    url = "https://olcha.uz/ru"
+def fetch_car_data(url):
     response = requests.get(url)
-    response.raise_for_status()
-
     soup = BeautifulSoup(response.text, 'html.parser')
-
-    # Mahsulotlarni topish (sahifa tuzilishiga qarab o'zgartirish kerak bo'lishi mumkin)
-    products = soup.find_all('div', class_='product-item')
-
-    product_list = []
-    for product in products:
-        try:
-            name = product.find('a', class_='product-title').text.strip()
-            price = product.find('div', class_='product-price').text.strip()
-            description = product.find('div', class_='product-description').text.strip()
-        except AttributeError:
-            continue
-
-        product_list.append({
-            "name": name,
-            "price": price,
-            "description": description
-        })
-
-    return product_list
-
-# Mahsulotlarni backendga yuborish
-def send_products_to_backend(product_list):
-    endpoint_url = "http://127.0.0.1:8000/products/"  # Backend endpoint URL
-
-    for product in product_list:
-        data = {
-            "name": product['name'],
-            "price": product['price'],
-            "description": product['description']
+    
+    table_rows = soup.find_all('tr')
+    
+    cars_data = []
+    for row in table_rows[1:]:  
+        columns = row.find_all('td')
+        car_info = {
+            "date": columns[0].text.strip(),
+            "car": columns[1].text.strip(),
+            "model": columns[2].text.strip(),
+            "transmission": columns[3].text.strip(),
+            "transmission_type": columns[4].text.strip(),
+            "status": columns[5].text.strip(),
         }
+        cars_data.append(car_info)
+    
+    return cars_data
 
-        response = requests.post(endpoint_url, json=data)
-        if response.status_code == 201:
-            print(f"Mahsulot yaratildi: {product['name']}")
-        else:
-            print(f"Xato: {response.status_code}, Mahsulot: {product['name']}")
 
-if __name__ == "__main__":
-    products = get_products_from_olcha()
-    send_products_to_backend(products)
+all_data = []
+for i in range(1,9):
+    car_data_url = f'https://www.carmodoo.com/consult/consultList.html?page={i}&myConsult=&id='
+    car_data = fetch_car_data(car_data_url)
+    for i in car_data:
+        all_data.append(i)
+
+# Saving the data to a JSON file
+with open('car_data.json', 'w', encoding='utf-8') as f:
+    json.dump(all_data, f, ensure_ascii=False, indent=4)
+
+print("Data has been successfully saved to car_data.json.")
